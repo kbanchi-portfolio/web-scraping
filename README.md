78a5fb94af799a0609031de4912a631498d4a51c
kbanchi1111 <kbanchi.1111@gmail.com>
2021-10-12 13:41:26 +0900

    write help and add requirements.txt

--- README.md
+++ README.md
@@ -11,14 +11,44 @@
 
 ## About The Project
 
+PythonでのWebスクレイピングについて、いくつかの手法をまとめたプロジェクトです。
+
+* [Beautiful Soup](./beautiful_soup/)
+  * Beautiful Soupを用いて、指定したカテゴリに応じた記事の一覧を取得する
+* [Flickr API](./flickrapi/)
+  * Flickr APIを用いて、指定したカテゴリの画像を、指定した枚数分だけ取得し保存する
+* [Selenium](./selenium/)
+  * Seleniumを用いて、指定したカテゴリの画像を、指定した枚数分だけ取得し保存する
+
 ## Getting Start
+
+本プロジェクトをローカルで実行するために必要なことを記載します。
 
 ### Prerequisites
 
+* Python
+```
+$ python --version
+Python 3.9.7
+```
+
 ### Installation
+
+1. プロジェクトのClone
+```
+$ git clone git@github.com:kbanchi-portfolio/web-scraping.git
+```
+2. 必要パッケージのインストール
+```
+$ pip install -r requirements.txt
+```
 
 ## Usage
 
-## Note
+各モジュールの使い方は、ヘルプをご参照ください。
 
 ## Contact
+
+kbanchi - [@kbanchi11](https://twitter.com/kbanchi11) - kbanchi.1111@gmail.com
+
+Project Link: [https://github.com/kbanchi-portfolio/](https://github.com/kbanchi-portfolio/)
--- flickrapi/flickr_images.py
+++ flickrapi/flickr_images.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Web scraping using Flickr API.
+This is a scraping sample using Flickr API.
+Flickr is a web site to share a lot of kind of photos.
+If you use Flickr API, you can do scraping and get photos from Flickr.
+You have to ready API key and secret from Flickr to run this module.
+Please write API key and secret into api_key.yaml.
+And also, please specify target of photo category and number of download photos
+when you running this module.
+
+Example:
+    $ python flickr_images.py [target] [number of photos]
+"""
 import os
 import shutil
 import sys
@@ -7,57 +20,68 @@ import urllib.request
 import yaml
 from flickrapi import FlickrAPI
 
-# get API Key from yaml
-key = ''
-secret = ''
-try:
-    with open('api_key.yaml', 'r') as f:
-        data = yaml.load(f, Loader=yaml.SafeLoader)
-        key = data['API_KEY']['key']
-        secret = data['API_KEY']['secret']
-except FileNotFoundError as e:
-    print(e)
-    exit()
 
-# check argument if specify target animal
-target = ''
-per_page = 0
-try:
-    target = sys.argv[1]
-    per_page = int(sys.argv[2])
-except IndexError:
-    print('Please specify target(str) and number of images(int).')
-    exit()
-except ValueError:
-    print('Please specify target(str) and number of images(int).')
-    exit()
+def main():
+    """Web scraping main function.
+    Scraping using Flickr API and download photos of target.
+    Downloaded photos are saved in images/ directory.
+    :return:
+    """
+    # get API Key from yaml
+    key = ''
+    secret = ''
+    try:
+        with open('api_key.yaml', 'r') as f:
+            data = yaml.load(f, Loader=yaml.SafeLoader)
+            key = data['API_KEY']['key']
+            secret = data['API_KEY']['secret']
+    except FileNotFoundError as e:
+        print(e)
+        exit()
 
-# make target directory (if it's exist, remove and remake directory)
-save_dir = 'images/' + target
-if not os.path.exists(save_dir):
-    os.makedirs(save_dir)
-else:
-    shutil.rmtree(save_dir)
-    os.makedirs(save_dir)
+    # check argument if specify target animal
+    target = ''
+    per_page = 0
+    try:
+        target = sys.argv[1]
+        per_page = int(sys.argv[2])
+    except IndexError:
+        print('Please specify target(str) and number of images(int).')
+        exit()
+    except ValueError:
+        print('Please specify target(str) and number of images(int).')
+        exit()
 
-# call FlickrAPI
-flickr = FlickrAPI(api_key=key, secret=secret, format='parsed-json')
-response = flickr.photos.search(
-    text=target,
-    per_page=per_page,
-    media='photos',
-    sort='relevance',
-    safe_search=1,
-    extras='url_q, licence'
-)
+    # make target directory (if it's exist, remove and remake directory)
+    save_dir = 'images/' + target
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    else:
+        shutil.rmtree(save_dir)
+        os.makedirs(save_dir)
 
-# get images from API response
-wait_time = 1
-for image in response['photos']['photo']:
-    save_file = save_dir + '/' + image['id'] + '.jpg'
-    if os.path.exists(save_file):
-        continue
-    data = urllib.request.urlopen(image['url_q']).read()
-    with open(save_file, 'wb') as f:
-        f.write(data)
-    time.sleep(wait_time)
+    # call FlickrAPI
+    flickr = FlickrAPI(api_key=key, secret=secret, format='parsed-json')
+    response = flickr.photos.search(
+        text=target,
+        per_page=per_page,
+        media='photos',
+        sort='relevance',
+        safe_search=1,
+        extras='url_q, licence'
+    )
+
+    # get images from API response
+    wait_time = 1
+    for image in response['photos']['photo']:
+        save_file = save_dir + '/' + image['id'] + '.jpg'
+        if os.path.exists(save_file):
+            continue
+        data = urllib.request.urlopen(image['url_q']).read()
+        with open(save_file, 'wb') as f:
+            f.write(data)
+        time.sleep(wait_time)
+
+
+if __name__ == '__main__':
+    main()
