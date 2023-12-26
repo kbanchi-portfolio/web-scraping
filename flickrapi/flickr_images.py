df47109cdb368be7c3fc3007ded17aaf7c2c8660
kbanchi1111 <kbanchi.1111@gmail.com>
2023-12-26 16:55:05 +0900

    fix flickr api

--- flickrapi/flickr_images.py
+++ flickrapi/flickr_images.py
@@ -28,32 +28,32 @@ def main():
     :return:
     """
     # get API Key from yaml
-    key = ''
-    secret = ''
+    key = ""
+    secret = ""
     try:
-        with open('api_key.yaml', 'r') as f:
+        with open("api_key.yaml", "r") as f:
             data = yaml.load(f, Loader=yaml.SafeLoader)
-            key = data['API_KEY']['key']
-            secret = data['API_KEY']['secret']
-    except FileNotFoundError as e:
-        print(e)
+            key = data["API_KEY"]["key"]
+            secret = data["API_KEY"]["secret"]
+    except FileNotFoundError:
+        print("api_key.yaml does not exists.")
         exit()
 
     # check argument if specify target animal
-    target = ''
+    target = ""
     per_page = 0
     try:
         target = sys.argv[1]
         per_page = int(sys.argv[2])
     except IndexError:
-        print('Please specify target(str) and number of images(int).')
+        print("Please specify target(str) and number of images(int).")
         exit()
     except ValueError:
-        print('Please specify target(str) and number of images(int).')
+        print("Please specify target(str) and number of images(int).")
         exit()
 
     # make target directory (if it's exist, remove and remake directory)
-    save_dir = 'images/' + target
+    save_dir = f"images/{target}"
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
     else:
@@ -61,27 +61,32 @@ def main():
         os.makedirs(save_dir)
 
     # call FlickrAPI
-    flickr = FlickrAPI(api_key=key, secret=secret, format='parsed-json')
-    response = flickr.photos.search(
-        text=target,
-        per_page=per_page,
-        media='photos',
-        sort='relevance',
-        safe_search=1,
-        extras='url_q, licence'
-    )
+    try:
+        flickr = FlickrAPI(api_key=key, secret=secret, format="parsed-json")
+        response = flickr.photos.search(
+            text=target,
+            per_page=per_page,
+            media="photos",
+            sort="relevance",
+            safe_search=1,
+            extras="url_q, licence",
+        )
+    except Exception:
+        print("Occured any connection error.")
+        exit()
 
     # get images from API response
     wait_time = 1
-    for image in response['photos']['photo']:
-        save_file = save_dir + '/' + image['id'] + '.jpg'
+    for image in response["photos"]["photo"]:
+        save_file = f"{save_dir}/{image['id']}.jpg"
         if os.path.exists(save_file):
             continue
-        data = urllib.request.urlopen(image['url_q']).read()
-        with open(save_file, 'wb') as f:
+        with urllib.request.urlopen(image["url_q"]) as u:
+            data = u.read()
+        with open(save_file, "wb") as f:
             f.write(data)
         time.sleep(wait_time)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
